4502384746e7782220d7e2b547f4955288780196
Ben Golden & WarpDrive McNate <pair+bengolden.natekandler@devbootcamp.com>
2014-05-23 15:56:01 -0500

    created index page and shifted login interface

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -1,5 +1,6 @@
 get '/' do
   # Look in app/views/index.erb
+  @surveys = Survey.all
   erb :index
 end
 
@@ -9,4 +10,21 @@ post '/sessions/new' do
     session[:user_id] = @user.id  #creates session
   end
   redirect '/'
+end
+
+delete '/logout' do
+  session[:user_id] = nil
+  redirect '/'
+end
+
+get '/signup' do
+  erb :"/users/new"
+end
+
+post '/users/new' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:user_id] = @user.id
+    redirect "/user/profile"
+  end
 end
