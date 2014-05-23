52a4edec0b1bc64841a75f353a2980e58cb2e196
Fabi & Parth Naik <pair+fab9.parthnaik@devbootcamp.com>
2014-05-23 17:08:12 -0500

    complete profile page

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -5,8 +5,30 @@ end
 
 post '/sessions/new' do
   if User.authenticate(params[:email], params[:password])
-    @user = User.find_by_email(params[:email])
-    session[:user_id] = @user.id  #creates session
+    user = User.find_by_email(params[:email])
+    session[:user_id] = user.id  #creates session
+    redirect ('/')
+  else
+    redirect ('/')
   end
-  redirect '/'
+end
+
+get '/user/profile' do
+    @surveys_created = current_user.created_surveys
+    @surveys_taken = current_user.taken_surveys
+    erb :'users/profile'
+end
+
+#================= Show survey
+get '/surveys/new' do
+
+    erb :'surveys/new'
+end
+
+post '/surveys/new' do
+  @survey = Survey.create(
+    name: params[:survey_name],
+    creator_id: current_user.id)
+
+    redirect :'users/profile'
 end
