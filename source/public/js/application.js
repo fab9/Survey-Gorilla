723d9cb626239e80dfd9bb8c46941bc41c1eb1eb
Fabi & Parth Naik <pair+fab9.parthnaik@devbootcamp.com>
2014-05-23 19:21:56 -0500

    gets Jquery working for questions but not choices

--- source/public/js/application.js
+++ source/public/js/application.js
@@ -1,7 +1,26 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+var question_number = 0;
+var choice_number = [0]
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-});
+
+
+$(document).ready(function() {
+  $("#add_question").on("click", function(event){
+    event.preventDefault();
+    question_number += 1;
+    //console.log($("#question_field"));
+    // console.log(existingdiv1);
+    // question = $("#quesiton");
+    // $("#question").append(question, question);
+    $("#question").append("<div id=\"question\"><label id=\"question_field\">question<input type=\"text\" name=\"question[" + question_number + "][question]\"></label><div id=\"choice\"><label>choice<input type=\"text\" name=\"choice[0][choice]\"></label></div><a href=\"#\" id=\"add_choice\">add choice</a></div> ");
+  });
+
+
+  $("#add_choice").on("click", function(event){
+    event.preventDefault();
+    //console.log($("#question_field"));
+    // console.log(existingdiv1);
+    // question = $("#quesiton");
+    // $("#question").append(question, question);
+    $("#choice").append("<div id=\"choice\"><label>choice<input type=\"text\" name=\"choice[" + +"][choice]\"></label></div> ");
+    });
+  });
